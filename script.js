3a2a99abba2a4610d364c48b27e255ca235d8bf7
Mobin Akhter <akhtemob@sheridancollege.ca>
2024-03-18 21:33:08 -0400

    created the landing page, basic.

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example: Adding a simple interactive feature, like alerting when the "Buy Me a Coffee" button is clicked.
+document.querySelector("#support a").addEventListener("click", function () {
+  alert("Thank you for considering to support us!");
+});
